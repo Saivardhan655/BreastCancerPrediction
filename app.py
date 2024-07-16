a212b0fae62a9d5a0dec932f937459876189555e
Saivardhan655 <saivardhan9154@gmail.com>
2024-07-16 12:57:40 +0530

    The web app is ready\

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the model
 reg_model = pickle.load(open('breast_cancer.pkl', 'rb'))
 
-# Mapping of the numerical output to the corresponding diagnosis
 q = {1: 'Malignant', 0: 'Benign'}
 
 @app.route('/')
@@ -18,17 +16,23 @@ def home():
 def prediction_api():
     data = request.json['data']
     print(data)
-    
-    # Convert the input data into the correct shape for prediction
-    new_data = np.array(list(data.values())).reshape(1, -1)  # Reshape to (1, number_of_features)
+
+    new_data = np.array(list(data.values())).reshape(1, -1)
     print(new_data)
     
-    # Predict using the loaded model
     output = reg_model.predict(new_data)
     
-    # Map the output to the corresponding diagnosis and return as JSON
     prediction = q[output[0]]
     return jsonify({'prediction': prediction})
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    data=np.array(data)
+    output = reg_model.predict(data.reshape(1,-1))
+    prediction = q[output[0]]
+    return render_template("home.html",prediction_text="The predicted is {}".format(prediction))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
